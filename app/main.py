122d31055cadece7ea3e698e6c9ddd56de7128eb
Ethicaldisguise <7862sadikd@gmail.com>
2024-09-24 20:55:46 +0530

    .

--- app/main.py
+++ app/main.py
@@ -2,22 +2,32 @@ import socket
 
 MAX_DATAGRAM_SIZE = 512 * 1024
 
+
+def resolve_packet(req_data: bytes):
+    """
+    
+    """
+
+
+def responce(req_packet):
+    ...
+
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this block to pass the first stage
-    #
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.bind(("127.0.0.1", 2053))
 
     while True:
         try:
-            buf, source = udp_socket.recvfrom(512)
-            print(buf)
-            response = b""
-
+            packet, source = udp_socket.recvfrom(MAX_DATAGRAM_SIZE)
+            print("request from", source)
+            resolved_packet = resolve_packet(packet)
+            response = responce(resolved_packet)
             udp_socket.sendto(response, source)
+
         except Exception as e:
             print(f"Error receiving data: {e}")
             break

d5881046c5a7ed000dfce2e9417fbfe925c881f4
Ethicaldisguise <7862sadikd@gmail.com>
2024-09-24 20:46:53 +0530

    .

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import socket
 
+MAX_DATAGRAM_SIZE = 512 * 1024
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,13 +10,13 @@ def main():
     #
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.bind(("127.0.0.1", 2053))
-    
+
     while True:
         try:
             buf, source = udp_socket.recvfrom(512)
-    
+            print(buf)
             response = b""
-    
+
             udp_socket.sendto(response, source)
         except Exception as e:
             print(f"Error receiving data: {e}")

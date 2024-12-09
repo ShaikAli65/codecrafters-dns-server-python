4c6c8a899791fccee364d0763d36e380986b1631
Ethicaldisguise <7862sadikd@gmail.com>
2024-12-09 22:12:25 +0530

    .

--- app/main.py
+++ app/main.py
@@ -1,12 +1,30 @@
 import socket
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 import struct
 
 MAX_DATAGRAM_SIZE = 512 * 1024
 
-@dataclass
+@dataclass(slots=True)
 class DnsHeader:
-    "DNS Header total size 12 bytes"
+    """
+    Packet Identifier (ID)	            16 bits     A random ID assigned to query packets.
+    --- Flags ---
+    Query/Response Indicator (QR)	    1 bit	    1 for a reply packet, 0 for a question packet.
+    Operation Code (OPCODE)	            4 bits	    Specifies the kind of query in a message.
+    Authoritative Answer (AA)	        1 bit	    1 if the responding server "owns" the domain queried, i.e., it's authoritative.
+    Truncation (TC)	                    1 bit	    1 if the message is larger than 512 bytes. Always 0 in UDP responses.
+    Recursion Desired (RD)	            1 bit	    Sender sets this to 1 if the server should recursively resolve this query, 0 otherwise.
+    Recursion Available (RA)    	    1 bit	    Server sets this to 1 to indicate that recursion is available.
+    Reserved (Z)                	    3 bits      Used by DNSSEC queries
+    Response Code (RCODE)	            4 bits	    Response code indicating the status of the response.
+    ---
+    
+    Question Count (QDCOUNT)	        16 bits	    Number of questions in the Question section.
+    Answer Record Count (ANCOUNT)	    16 bits	    Number of records in the Answer section.
+    Authority Record Count (NSCOUNT)	16 bits	    Number of records in the Authority section.
+    Additional Record Count (ARCOUNT)	16 bits	    Number of records in the Additional section.
+
+    """
     ID: int
     QR: bool
     OPCODE: int
@@ -26,7 +44,7 @@ class DnsHeader:
                 (self.TC << 9) | (self.RD << 8) | (self.RA << 7) | \
                 (self.Z << 4) | self.RCODE
 
-        return struct.pack(
+        return struct.pack( 
             "!6H",
             self.ID,
             flags,
@@ -44,7 +62,7 @@ class DNSRR:
     TYPE: int
     CLASS: int 
     TTL: int
-    RDLENGTH: int 
+    RDLENGTH: int
     RDATA: int
 
 
@@ -61,6 +79,8 @@ def resolve_packet(req_data: bytes):
     Recursion Available (RA)    	    1 bit	    Server sets this to 1 to indicate that recursion is available.
     Reserved (Z)                	    3 bits      Used by DNSSEC queries
     Response Code (RCODE)	            4 bits	    Response code indicating the status of the response.
+    ---
+
     Question Count (QDCOUNT)	        16 bits	    Number of questions in the Question section.
     Answer Record Count (ANCOUNT)	    16 bits	    Number of records in the Answer section.
     Authority Record Count (NSCOUNT)	16 bits	    Number of records in the Authority section.
@@ -69,7 +89,7 @@ def resolve_packet(req_data: bytes):
     total length 12 bytes
 
     --- Resource Record ---
-    @wikipedia
+    @https://en.wikipedia.org/wiki/Domain_Name_System
     NAME	    Name of the node to which this record pertains	Variable
     TYPE	    Type of RR in numeric form (e.g., 15 for MX RRs)	                                        2
     CLASS	    Class code	                                                                                2
@@ -82,12 +102,12 @@ def resolve_packet(req_data: bytes):
     flags = unpacket_tuple[1]
     qr = flags >> 15  # bit 16
     op_code = (flags >> 11) & 0b1111  # OPCODE: bits 12-15
-    aa = (flags >> 10) & 0b1  # AA: bit 11
-    tc = (flags >> 9) & 0b1  # TC: bit 10
-    rd = (flags >> 8) & 0b1  # RD: bit 9
-    ra = (flags >> 7) & 0b1  # RA: bit 8
-    z = (flags >> 4) & 0b111  # Z: bits 5-7
-    rcode = flags & 0b1111  # RCODE: bits 1-4
+    aa = (flags >> 10) & 0b1          # AA: bit 11
+    tc = (flags >> 9) & 0b1           # TC: bit 10
+    rd = (flags >> 8) & 0b1           # RD: bit 9
+    ra = (flags >> 7) & 0b1           # RA: bit 8
+    z = (flags >> 4) & 0b111          # Z: bits 5-7
+    rcode = flags & 0b1111            # RCODE: bits 1-4
     qdcount = unpacket_tuple[2]
     ancount = unpacket_tuple[3] 
     nscount = unpacket_tuple[4]
@@ -108,10 +128,8 @@ def main():
     while True:
         packet, source = udp_socket.recvfrom(MAX_DATAGRAM_SIZE)
         print("request from", source)
-        print("packet", packet)
         resolved_packet = resolve_packet(packet)
         # response = responce(resolved_packet)
-        resolved_packet.QR = 1
         print("replying with", resolved_packet)
         udp_socket.sendto(bytes(resolved_packet), source)
 

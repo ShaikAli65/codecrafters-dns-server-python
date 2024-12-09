9a3068c7311361d5540883f443ae9f4daf9ea3b1
Ethicaldisguise <7862sadikd@gmail.com>
2024-12-10 00:25:16 +0530

    .

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import enum
 import socket
 from dataclasses import dataclass
 import struct
@@ -55,6 +56,20 @@ class DnsHeader:
             self.ARCOUNT
         )
 
+class OPCODE(enum.IntEnum):
+    STANDARD = 0
+    INVERSE = 1
+    STATUS = 2
+    FUT = enum.auto()
+
+
+class RCODE(enum.IntEnum):
+    NOERROR = 0
+    FORMAT_ERR = 1
+    SERVER_FAIL = 2
+    NAME_ERR = 3
+    NOT_IMPL = 4
+    REFUSED = 5
 
 @dataclass(slots=True)
 class DNSRR:
@@ -84,6 +99,37 @@ class DNSRR:
         mid = struct.pack('!HHIH', self.TYPE, self.CLASS, self.TTL, self.RDLENGTH)
         end = self.RDATA
         return start + mid + end
+        
+
+class TYPE(enum.Enum):
+    A     = 1   # a host address
+    NS    = 2   # an authoritative name server
+    MD    = 3   # a mail destination (Obsolete - use MX)
+    MF    = 4   # a mail forwarder (Obsolete - use MX)
+    CNAME = 5   # the canonical name for an alias
+    SOA   = 6   # marks the start of a zone of authority
+    MB    = 7   # a mailbox domain name (EXPERIMENTAL)
+    MG    = 8   # a mail group member (EXPERIMENTAL)
+    MR    = 9   # a mail rename domain name (EXPERIMENTAL)
+    NULL  = 10  # a null RR (EXPERIMENTAL)
+    WKS   = 11  # a well known service description
+    PTR   = 12  # a domain name pointer
+    HINFO = 13  # host information
+    MINFO = 14  # mailbox or mail list information
+    MX    = 15  # mail exchange
+    TXT   = 16  # text strings
+    AXFR = 252  # A request for a transfer of an entire zone
+    MAILB = 253  # A request for mailbox-related records (MB, MG or MR)
+    MAILA = 254  # A request for mail agent RRs (Obsolete - see MX)
+    ALL = 255  # any
+
+
+class CLASS(enum.IntEnum):
+    IN = 1  # the Internet
+    CS = 2  # the CSNET class (Obsolete - used only for examples in some obsolete RFCs)
+    CH = 3  # the CHAOS class
+    HS = 4  # Hesiod [Dyer 87]
+    ALL = 255  # any
 
 @dataclass(slots=True)
 class Question:
@@ -148,16 +194,17 @@ def resolve_domain(packet: bytes):
             break 
     return domain, parts, packet[12: ][e + 1: ]
 
-def resolve_question(packet):
+def resolve_questions(header: DnsHeader, packet):
+    header.QDCOUNT
     *parts, everthing_else = resolve_domain(packet)
     return Question(*parts, *struct.unpack('!HH',everthing_else))
 
 def responce(header: DnsHeader, question: Question):
+    resp = DNSRR(question.raw_name, question.QNAME, question.QTYPE, question.QCLASS, 0, 0, b'')
+    
     header.QR = True
-    header.QDCOUNT = 1
-    resp = DNSRR(question.raw_name,question.QNAME,question.QTYPE,question.QCLASS,0,0, b'')
     header.ANCOUNT = 1
-    header.RCODE = 4 # not implemented
+    header.RCODE = RCODE.NOT_IMPL
     print(header,'\n', question,'\n', resp)
     return bytes(header) + bytes(question) + bytes(resp)
 
@@ -171,7 +218,7 @@ def main():
     while True:
         packet, source = udp_socket.recvfrom(MAX_DATAGRAM_SIZE)
         resolved_header = resolve_header(packet)
-        question = resolve_question(packet)
+        question = resolve_questions(resolved_header, packet)
         print("received", packet)
         print("resolved header", resolved_header)
         print(resp := responce(resolved_header, question))
